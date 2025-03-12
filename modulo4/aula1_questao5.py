39273d65afcb317c70de67e4cdb5926d47ea7d05
Aline Bervian <abbervian@gmail.com>
2025-03-12 17:34:27 -0300

    commit questões 2 modulo 4 -for até continue

--- modulo4/aula1_questao5.py
+++ modulo4/aula1_questao5.py
@@ -7,8 +7,8 @@ cont = 0
 #processamento e entrada
 while cont <n_respostas:
     idade = int(input("idade: "))
-    soma += idade
-    cont += 1
+    soma = soma + idade
+    cont = cont+1
 
 #saida
 print (f"A idade média dos {n_respostas} respondentes é de {int (soma/n_respostas): .0f} anos")
